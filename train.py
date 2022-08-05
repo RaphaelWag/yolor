2f60c4dc9f787601edf9396939a5b82d4f007ef2
RaphealWagSCCH <Raphael.Wagner@scch.at>
2022-08-05 14:35:55 +0200

    bugfix

--- train.py
+++ train.py
@@ -291,7 +291,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
         # b = int(random.uniform(0.25 * imgsz, 0.75 * imgsz + gs) // gs * gs)
         # dataset.mosaic_border = [b - imgsz, -b]  # height, width borders
 
-        mloss = torch.zeros(5, device=device)  # mean losses
+        mloss = torch.zeros(7, device=device)  # mean losses
         if rank != -1:
             dataloader.sampler.set_epoch(epoch)
         pbar = enumerate(dataloader)
