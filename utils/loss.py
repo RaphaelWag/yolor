bfd2fee437fbefe514530f3a0c47a99149b8d0cf
RaphealWagSCCH <Raphael.Wagner@scch.at>
2022-10-18 15:32:25 +0200

    harcoding predicted bounding boxes

--- utils/loss.py
+++ utils/loss.py
@@ -107,6 +107,7 @@ def compute_loss(p, targets, model):  # predictions, targets, model
             # Box Regression
             pxy = ps[:, :2].sigmoid() * 2. - 0.5
             pwh = (ps[:, 2:4].sigmoid() * 2) ** 2 * anchors[i]
+            pwh = torch.ones(size=pwh.shape) * h['box_size']
             pbox = torch.cat((pxy, pwh), 1).to(device)  # predicted box
             iou = bbox_iou(pbox.T, tbox[i], x1y1x2y2=False, CIoU=True)  # iou(prediction, target)
             lbox += (1.0 - iou).mean()  # iou loss
